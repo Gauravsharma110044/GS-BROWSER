c86949923aa899c692e5be969a914a194e494976
Chetan3327 <chauhanchetan12789@gmail.com>
2024-05-07 18:06:29 +0530

    edit chat name

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -27,3 +27,33 @@ export async function DELETE(req:Request, {params}: {params: {chatId: string}}){
     return NextResponse.json({message: 'Error deleting chat'}, {status: 500})
   }  
 }
+
+export async function PATCH(req:Request, {params}: {params: {chatId: string}}){
+  try {
+    const user = await currentUser()
+    if(!user){
+      return new NextResponse('Unauthorized', {status: 401})
+    }
+
+    if(!params.chatId){
+      return new NextResponse('chatId not provided', {status: 401})
+    }
+
+    const {name} = await req.json()
+    
+    const chat = await prisma.chat.update({
+      where: {
+        id: params.chatId,
+        userId: user.id
+      },
+      data: {
+        name
+      }
+    })
+
+    return NextResponse.json(chat)
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({message: 'Error deleting chat'}, {status: 500})
+  }  
+}
--- components/sidebar/sidebar-item.tsx
+++ components/sidebar/sidebar-item.tsx
@@ -2,16 +2,20 @@
 import { Chat } from '@prisma/client'
 import { Archive, MoreHorizontal, Pencil, Share, Trash } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu'
 import { useModal } from '@/hooks/use-modal-store'
+import { SideBarItemEdit } from './sidebar-name-edit-form'
 
 const SideBarItem = ({chat}: {chat: Chat}) => {
+  const [isEditing, setIsEditing] = useState(false)
   const router = useRouter()
   const {onOpen} = useModal()
+
   return (
     <button key={chat.id} onClick={() => router.push(`/${chat.id}`)} className='group p-2 rounded-md flex items-center gap-x-3 w-full hover:bg-zinc-700/30 transition'>
-      <p className='text-sm'>{chat.name}</p>
+      {!isEditing && (<p className='text-sm'>{chat.name}</p>)}
+      {isEditing && (<SideBarItemEdit chat={chat} setIsEditing={setIsEditing} />)}
       <div className='space-x-2 ml-auto flex'>
         <DropdownMenu>
           <DropdownMenuTrigger className="focus:outline-none" asChild>
@@ -21,7 +25,7 @@ const SideBarItem = ({chat}: {chat: Chat}) => {
             <DropdownMenuItem className='px-3 p-2 text-sm cursor-pointer'>
               <Share className='flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2'/> Share
             </DropdownMenuItem>
-            <DropdownMenuItem className='px-3 p-2 text-sm cursor-pointer'>
+            <DropdownMenuItem onClick={() => setIsEditing(true)} className='px-3 p-2 text-sm cursor-pointer'>
               <Pencil className='flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2'/> Rename
             </DropdownMenuItem>
             <DropdownMenuItem onClick={() => onOpen('deleteChat', {chat})} className='px-3 p-2 text-sm cursor-pointer'>
